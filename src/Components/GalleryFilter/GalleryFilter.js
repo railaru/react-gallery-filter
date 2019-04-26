a56dac32ca7f44e2abdc08528cc6833c3f882b42
Rutenis Raila <rutenisraila@hotmail.com>
2019-04-26 07:43:34 +0100

    card style & top title updates

--- src/Components/GalleryFilter/GalleryFilter.js
+++ src/Components/GalleryFilter/GalleryFilter.js
@@ -1,23 +1,18 @@
-import React, { Component } from 'react'
-import Card from './Card'
-<<<<<<< HEAD
-import LabelCount from './LabelCount'
-import uuid from 'uuid'
-=======
->>>>>>> 26dc93fb58a7806d27e14ffb8a7d5fd3f908dac1
-import Checkbox from '@material-ui/core/Checkbox';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Loader from './Loader'
+import React, { Component } from "react";
+import Card from "./Card";
+import LabelCount from "./LabelCount";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Loader from "./Loader";
 
-import gallery_items from '../../gallery_items'
+import gallery_items from "../../gallery_items";
 
-let amountOfItems = 9
-const amountOfItemsIncrement = 6
+let amountOfItems = 9;
+const amountOfItemsIncrement = 3;
 
 export class GalleryFilter extends Component {
-
   constructor() {
-    super()
+    super();
 
     this.state = {
       isLoading: true,
@@ -25,29 +20,32 @@ export class GalleryFilter extends Component {
       courses: false,
       video: false,
       galleryItems: gallery_items.slice(0, amountOfItems)
-    }
+    };
   }
 
   handleAccordions() {
-    const accordionToggler = document.querySelectorAll('.accordion__top')
+    const accordionToggler = document.querySelectorAll(".accordion__top");
 
-    accordionToggler.forEach((toggler) => {
+    accordionToggler.forEach(toggler => {
       toggler.onclick = () => {
+        const openIcon = toggler.querySelector(
+          ".accordion__top__icon-container--open"
+        );
+        const closeIcon = toggler.querySelector(
+          ".accordion__top__icon-container--close"
+        );
 
-        const openIcon = toggler.querySelector('.accordion__top__icon-container--open')
-        const closeIcon = toggler.querySelector('.accordion__top__icon-container--close')
+        openIcon.classList.toggle("accordion__top__icon-container--disabled");
+        closeIcon.classList.toggle("accordion__top__icon-container--disabled");
 
-        openIcon.classList.toggle('accordion__top__icon-container--disabled')
-        closeIcon.classList.toggle('accordion__top__icon-container--disabled')
-
-
-        toggler.classList.toggle('accordion__top--opened')
-        toggler.parentNode.classList.toggle('accordion--opened')
-        toggler.parentNode.querySelector('.accordion__content').classList.toggle('accordion__content--opened')
-      }
-    })
+        toggler.classList.toggle("accordion__top--opened");
+        toggler.parentNode.classList.toggle("accordion--opened");
+        toggler.parentNode
+          .querySelector(".accordion__content")
+          .classList.toggle("accordion__content--opened");
+      };
+    });
   }
-
 
   onImagesLoaded(container, event) {
     var images = container.getElementsByTagName("img");
@@ -55,8 +53,7 @@ export class GalleryFilter extends Component {
     for (var i = 0; i < images.length; i++) {
       if (images[i].complete) {
         loaded--;
-      }
-      else {
+      } else {
         // eslint-disable-next-line
         images[i].onload = () => {
           loaded--;
@@ -71,37 +68,34 @@ export class GalleryFilter extends Component {
     }
   }
 
-
   handleLazyLoad() {
-
-    this.onImagesLoaded(document.querySelector('.container'), () => {      
+    this.onImagesLoaded(document.querySelector(".container"), () => {
       this.setState({ isLoading: false });
     });
 
     window.addEventListener("wheel", () => {
-
       this.setState({ isLoading: true });
-      
-      this.onImagesLoaded(document.querySelector('.container'), () => {        
+
+      this.onImagesLoaded(document.querySelector(".container"), () => {
         this.setState({ isLoading: false });
-      });      
+      });
 
       if (!this.state.isLoading)
-        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) { //reached bottom of the page
+        if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+          //reached bottom of the page
 
-          amountOfItems = amountOfItems + amountOfItemsIncrement
-
-          this.setState({ galleryItems: gallery_items.slice(0, amountOfItems) });         
+          amountOfItems = amountOfItems + amountOfItemsIncrement;
+          this.setState({
+            galleryItems: gallery_items.slice(0, amountOfItems)
+          });
         }
-
-    })
+    });
   }
 
   componentDidMount() {
-    this.handleAccordions()
-    this.handleLazyLoad()
+    this.handleAccordions();
+    this.handleLazyLoad();
   }
-
 
 
   handleChange = name => event => {
@@ -110,10 +104,12 @@ export class GalleryFilter extends Component {
   };
 
   render() {
-
     return (
-      <div className='gallery-filter'>
-        <div className='gallery-filter__left'>
+      <div className="gallery-filter">
+        <div className="gallery-filter__left">
+          <div className="gallery-filter__left__title font-1 color-8">
+            Filters
+          </div>
           <div className="accordion">
             <div className="accordion__top noselect">
               <div className="accordion__top__text">I am a...</div>
@@ -125,13 +121,13 @@ export class GalleryFilter extends Component {
               </div>
             </div>
             <div className="accordion__content noscrollbar">
-              <div className='accordion__content__field'>
+              <div className="accordion__content__field">
                 <FormControlLabel
                   control={
                     <Checkbox
                       checked={this.state.courses}
-                      onChange={this.handleChange('courses')}
-                      color='default'
+                      onChange={this.handleChange("courses")}
+                      color="default"
                       value="courses"
                     />
                   }
@@ -140,17 +136,20 @@ export class GalleryFilter extends Component {
                 <LabelCount count={this.state.galleryItems.length} />
               </div>
 
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={this.state.video}
-                    onChange={this.handleChange('video')}
-                    color='default'
-                    value="video"
-                  />
-                }
-                label="Video watcher"
-              />
+              <div className="accordion__content__field">
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={this.state.video}
+                      onChange={this.handleChange("video")}
+                      color="default"
+                      value="video"
+                    />
+                  }
+                  label="Video watcher"
+                />
+                <LabelCount count={this.state.galleryItems.length} />
+              </div>
             </div>
           </div>
           <div className="accordion">
@@ -164,88 +163,98 @@ export class GalleryFilter extends Component {
               </div>
             </div>
             <div className="accordion__content noscrollbar">
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={this.state.courses}
-                    onChange={this.handleChange('courses')}
-                    color='default'
-                    value="courses"
-                  />
-                }
-                label="Courses"
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={this.state.video}
-                    onChange={this.handleChange('video')}
-                    color='default'
-                    value="video"
-                  />
-                }
-                label="Video"
-              />
+              <div className="accordion__content__field">
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={this.state.courses}
+                      onChange={this.handleChange("courses")}
+                      color="default"
+                      value="courses"
+                    />
+                  }
+                  label="Courses"
+                />
+                <LabelCount count={this.state.galleryItems.length} />
+              </div>
+
+              <div className="accordion__content__field">
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={this.state.video}
+                      onChange={this.handleChange("video")}
+                      color="default"
+                      value="video"
+                    />
+                  }
+                  label="Videos"
+                />
+                <LabelCount count={this.state.galleryItems.length} />
+              </div>
             </div>
           </div>
         </div>
-        <div className='gallery-filter__right'>
-          {
-            this.state.galleryItems.map(item => {
-              if ((this.state.courses && item.type === 'course') || this.state.all) {
-                return (
-
-                  <Card
-                    key={item.id}
-                    link={item.link}
-                    img={item.img}
-                    type={item.type}
-                    title={item.title}
-                    description={item.description}
-                    pose={this.state.courses ? 'visible' : 'hidden'}
-                  />
-                )
-              }
-
-              if ((this.state.video && item.type === 'video') || this.state.all) {
-                return (
-                  <Card
-                    key={item.id}
-                    link={item.link}
-                    img={item.img}
-                    type={item.type}
-                    title={item.title}
-                    description={item.description}
-                    pose={this.state.video ? 'visible' : 'hidden'}
-                  />
-                )
-              }
-              if ((!this.state.courses && !this.state.video)) {
-                return (
-                  <Card
-                    key={item.id}
-                    link={item.link}
-                    img={item.img}
-                    type={item.type}
-                    title={item.title}
-                    description={item.description}
-                  />
-                )
-              }
-
-              return null
+        <div className="gallery-filter__right">
+          
+          <div className='gallery-filter__right__title font-1 color-8'>
+            {
+              `1 - ${9} of ${this.state.galleryItems.length} resources`                        
+            }            
+          </div>
+          <div className='gallery-filter__right__content'>
+          {this.state.galleryItems.map(item => {
+            if (
+              (this.state.courses && item.type === "course") ||
+              this.state.all
+            ) {
+              return (
+                <Card
+                  key={item.id}
+                  link={item.link}
+                  img={item.img}
+                  type={item.type}
+                  title={item.title}
+                  description={item.description}
+                  pose={this.state.courses ? "visible" : "hidden"}
+                />
+              );
             }
-            )
 
-          }
-          {
-            this.state.isLoading &&
-              <Loader/>
-          }
+            if ((this.state.video && item.type === "video") || this.state.all) {
+              return (
+                <Card
+                  key={item.id}
+                  link={item.link}
+                  img={item.img}
+                  type={item.type}
+                  title={item.title}
+                  description={item.description}
+                  pose={this.state.video ? "visible" : "hidden"}
+                />
+              );
+            }
+            if (!this.state.courses && !this.state.video) {
+              return (
+                <Card
+                  key={item.id}
+                  link={item.link}
+                  img={item.img}
+                  type={item.type}
+                  title={item.title}
+                  description={item.description}
+                />
+              );
+            }
+
+            return null;
+          })}
+          {this.state.isLoading && <Loader />}  
+          </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default GalleryFilter
+export default GalleryFilter;
